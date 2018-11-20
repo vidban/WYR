6b5c733b708d45fb40969d0ceb7145d2ea749998
vidhu bansal <liliput.j@gmail.com>
2018-11-20 02:24:16 -0800

    Refactored Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,59 +4,44 @@ import { NavLink } from 'react-router-dom'
 import { Redirect } from 'react-router-dom'
 import { removeAuthedUser } from '../actions/authedUser'
 
+const Nav = ({ authedUser, logOut}) => {
+    const navLinks = () => {
+        const links = [
+            ['/', 'Home'],
+            ['/add', 'New Question'],
+            ['/leaderboard', 'LeaderBoard']
+        ]
 
-class Nav extends Component {
-
-    handleClick = () => {
-        const { dispatch } = this.props
-        dispatch(removeAuthedUser('none'))
+        return links.map(link => (
+            <li key={link[1]}>
+                <NavLink to={link[0]} style = {{ textDecoration: 'none'}} >
+                <h2>{link[1]}</h2>
+                </NavLink>
+            </li>
+        ))
     }
-    render() {
-        const {authedUser} = this.props
-        console.log(this.props)
 
-        if (authedUser === 'none'){
-            return <Redirect to={{pathname:'/',
-            state: {} }}/>}
+    if (authedUser === 'none') return <Redirect to='/' />
 
-        return (
-            <nav className='nav'>
-                <ul>
-                    <li>
-                        <h1 className="app-logo">Would You Rather</h1>
-                    </li>
-                    <li>
-                    <NavLink to='/home' style={{textDecoration: 'none'}}>
-                        <h2>Home</h2>
-                    </NavLink>
-                    </li>
-
-                    <li>
-                    <NavLink to='/add' style={{textDecoration: 'none'}}>
-                        <h2>New Question</h2>
-                    </NavLink>
-                    </li>
-
-                    <li>
-                    <NavLink to='/leaderboard' style={{textDecoration: 'none'}}>
-                        <h2>LeaderBoard</h2>
-                    </NavLink>
-                    </li>
-
-                    <li>
-                        <h2 onClick={this.handleClick}>Logout</h2>
-                    </li>
-
-                </ul>
-            </nav>
-        )
-    }
+    return (
+        <nav className="nav">
+            <ul>
+                <li>
+                    <h1 className="app-logo">Would You Rather</h1>
+                </li>
+                {navLinks()}
+                <li>
+                    <h2 onClick= { () => logOut()}>Logout</h2>
+                </li>
+            </ul>
+        </nav>
+    )
 }
 
-function mapStateToProps({authedUser}) {
-    return {
-        authedUser,
-    }
-}
+const mapDispatchToProps = dispatch => ({
+    logOut: () => dispatch(removeAuthedUser('none'))
+})
 
-export default connect(mapStateToProps)(Nav)
+const mapStateToProps = ({ authedUser }) => ({ authedUser })
+
+export default connect(mapStateToProps, mapDispatchToProps)(Nav)
