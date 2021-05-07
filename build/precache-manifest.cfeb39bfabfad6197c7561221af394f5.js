f895ea58901a27d7aef1c9c91c7e610f0b5baa61
Vidhu <vidban22@gmail.com>
2021-05-07 16:42:00 -0700

    Misc. Modifications

---
+++ build/precache-manifest.cfeb39bfabfad6197c7561221af394f5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7cb62ce4db287c812fe",
+    "url": "/static/css/main.0bb2023f.chunk.css"
+  },
+  {
+    "revision": "a7cb62ce4db287c812fe",
+    "url": "/static/js/main.a7cb62ce.chunk.js"
+  },
+  {
+    "revision": "9d974b1d157ddc98f9c2",
+    "url": "/static/js/1.9d974b1d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d8f82d33aa74b44a2e7fdb91923a5cbf",
+    "url": "/static/media/moana.d8f82d33.png"
+  },
+  {
+    "revision": "eab8dcbcc600d76f94f7a8358d49e5c0",
+    "url": "/static/media/heihei.eab8dcbc.png"
+  },
+  {
+    "revision": "02ae8cabd7a547b81d2d94c72a03bb06",
+    "url": "/static/media/maui.02ae8cab.png"
+  },
+  {
+    "revision": "aa621b3da0d5a8521fd863a8d13f29f2",
+    "url": "/static/media/spiral.aa621b3d.png"
+  },
+  {
+    "revision": "0b8deac330a2e162b823c693f5965634",
+    "url": "/index.html"
+  }
+];
